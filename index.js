31c6f22f204bd8bd3643ccbe628522b7d347ec01
Norrheden <eliasnorrhedenmau@gmail.com>
2025-01-10 01:32:12 +0100

    Letters and symbols

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ let pElement = document.querySelector("p")
 let askLetters = prompt("How many letters");
 Number(askLetters);
 
+let askSymbols = prompt("How many symbols");
+
 let randIndex;
+let randIndexOfArray;
+
 
 for(let i = 0; i< askLetters; i++) {
     randIndex = minMaxRand(0, letters.length - 1)
@@ -16,6 +20,20 @@ for(let i = 0; i< askLetters; i++) {
 
     }
 }
+let arrayOfLetters = pElement.textContent.split("")
+
+for(let i = 0; i < askSymbols; i++) {
+    randIndexOfArray = minMaxRand(0, arrayOfLetters.length);
+    randIndex = minMaxRand(0, symbols.length - 1)
+    arrayOfLetters.splice(randIndexOfArray, 0, `${symbols[randIndex]}`)
+
+
+}
+
+let password = arrayOfLetters.join("")
+pElement.textContent = `${password}`
+
+
 
 
 function minMaxRand(min, max) {
